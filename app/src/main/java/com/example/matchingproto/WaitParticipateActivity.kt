29f362cfe6bbf9dbe24a10a937290f6fd590eb90
gergese <jeffsong16@gmail.com>
2023-06-09 16:50:56 +0900

    매칭 취소

--- app/src/main/java/com/example/matchingproto/WaitParticipateActivity.kt
+++ app/src/main/java/com/example/matchingproto/WaitParticipateActivity.kt
@@ -3,11 +3,16 @@ package com.example.matchingproto
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.matchingproto.databinding.WaitParticipantsBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 class WaitParticipateActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -23,6 +28,10 @@ class WaitParticipateActivity : AppCompatActivity(), OnMapReadyCallback {
         myID=intent.getStringExtra("myID").toString()
         binding=WaitParticipantsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imgbBack.setOnClickListener {
+            showExitConfirmationDialog()
+        }
         waitParticipant()
         (supportFragmentManager.findFragmentById(R.id.mapView) as
                 SupportMapFragment?)!!.getMapAsync(this)
@@ -88,6 +97,42 @@ class WaitParticipateActivity : AppCompatActivity(), OnMapReadyCallback {
                     Log.d("Firestore", "Document does not exist")
                 }
             }
+    }
+
+    //매칭 취소 여부 확인 창
+    private fun showExitConfirmationDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("매칭 종료 확인")
+            .setMessage("매칭을 취소하시겠습니까?")
+            .setPositiveButton("예") { dialog, which ->
+                // '예' 버튼을 클릭한 경우 종료 로직을 구현
+                val task1 = partyDB.collection("party")
+                    .whereEqualTo("organizerID",myID)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            document.reference.delete()
+                                .addOnSuccessListener {
+                                    // 문서 삭제 성공
+                                }
+                                .addOnFailureListener { exception ->
+                                    // 문서 삭제 실패
+                                }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // 문서 가져오기 실패
+                    }
+
+                val intent = Intent(this, Main_login::class.java)
+                startActivity(intent)
+                finish()
+                Toast.makeText(this, "매칭을 취소합니다.", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("아니오") { dialog, which ->
+                // '아니오' 버튼을 클릭한 경우 아무 동작 없음
+            }
+            .show()
     }
 
     override fun onMapReady(p0: GoogleMap?) {
